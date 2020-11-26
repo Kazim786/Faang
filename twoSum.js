d8132ac43a7dae4bd18370c73b0cd387eab88827
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-11-26 10:26:25 -0600

    wrote the brute force solution

---
+++ twoSum.js
@@ -0,0 +1,26 @@
+//First faang problem
+
+const numbs = [1, 3, 7, 9, 2];
+let target = 11
+
+
+
+const finder = (arr) => 
+{
+  const index = []
+  for(let i = 0; i < arr.length; i++)
+  {
+    for(let j = (i + 1); j < arr.length; j++)
+    {
+      if(arr[i] + arr[j] === target)
+      {
+        index.push(i, j)
+      }
+    }
+  }
+return index;
+}
+
+console.log(finder(numbs))
+
+//IT WORKS :D
