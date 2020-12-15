2b6bf881cbd12fade87be8eb95da25982040e908
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-12-14 21:07:30 -0600

    did twitter exercise

--- twoSum.js
+++ twoSum.js
@@ -34,4 +34,68 @@ console.log(finder(numbs, target))
 
 //IT WORKS :D
 
-//Brute force solution
+//Brute force solution^^
+
+//Opimized version
+
+const finder = (arr, t) => {
+    const numsMap = {};
+    for(let i = 0; i < numsMap.length; i++)
+    {
+        const currentMapVal = numsMap[arr[i]]
+        if(currentMapVal >= 0)
+        {
+            return 
+        }
+    }
+}
+
+//Find most recent and oldest tweet
+
+
+
+
+
+const array = [
+  {
+    tweet: "hi",
+    date: 2012
+  },
+  {
+    tweet: "UFC",
+    date: 2014
+  },
+  {
+    tweet: "IM A BIG FAN",
+    date: 2020
+  }
+]
+
+// console.log(array[0].tweet)
+
+const oldestAndRecent = (arr) => {
+  oldest = 2021
+  recent = 0
+  let datesStored = []
+  for(let i = 0; i < arr.length; i++)
+  {
+    if(arr[i].date < oldest)
+    {
+      oldest = arr[i].date;
+      
+      
+    }
+    if(arr[i].date > recent)
+    {
+      recent = arr[i].date;
+    }
+  }
+      datesStored.push(oldest)
+      datesStored.push(recent)
+
+      return datesStored
+  
+}
+//arr[i].date > recent
+
+console.log(oldestAndRecent(array))
